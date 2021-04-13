052585b4eb9181ef87f433589498ea4b79fcbe93
Xavier Pun <65343237+X3vvv@users.noreply.github.com>
2021-04-13 18:12:04 +0800

    Update hamming_distance.c

--- HammingDistance/hamming_distance.c
+++ HammingDistance/hamming_distance.c
@@ -1,3 +1,4 @@
+// implementation 1
 int hamming_distance(unsigned x, unsigned y) {
   int dist = 0;
   unsigned val = x ^ y;
@@ -11,4 +12,15 @@ int hamming_distance(unsigned x, unsigned y) {
   
   // Return the number of differing bits
   return dist;
+}
+
+// implementation 2
+int hamming_distance(unsigned x, unsigned y)
+{
+    return __builtin_popcount(x ^ y);
+}
+//if your compiler supports 64-bit integers
+int hamming_distance(unsigned long long x, unsigned long long y)
+{
+    return __builtin_popcountll(x ^ y);
 }
