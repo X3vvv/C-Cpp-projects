fffe036d049fdb0157497b750e9d77e4455ba887
Xavier Pun <65343237+X3vvv@users.noreply.github.com>
2021-04-13 18:43:29 +0800

    Delete crc16.c

--- crc16.c
+++
@@ -1,6 +0,0 @@
-unsigned short crc16(unsigned short crc, unsigned char *p, int len)
-{
-          while (len--)
-               crc = (crc >> 8) ^ crctab[(crc ^ *p++) & 0xff];
-          return crc;
-}
